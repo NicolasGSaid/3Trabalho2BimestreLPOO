9387cbc4a5d169019576b33ff3aa87b29a86d8e8
NicolasGSaid <nicolasgithub2@gmail.com>
2025-05-02 13:56:26 -0300

    a

--- SistemaFinanceiro/src/View/MenuFluxoCaixa.java
+++ SistemaFinanceiro/src/View/MenuFluxoCaixa.java
@@ -1,20 +1,61 @@
 package View;
 
+import controller.PagarController;
+import controller.ReceberController;
+import faker.PagarFaker;
+import faker.ReceberFaker;
+import model.PagarModel;
+import model.ReceberModel;
+
 public class MenuFluxoCaixa {
-    
-    public void menuFluxoCaixa(){
-    System.out.println("+----------------+------------+------------+------------+");
-        System.out.println("|                Fluxo de Caixa                |");
-        System.out.println("+----------------+------------+------------+------------+");
-        System.out.println("| Vencimento     | Crédito    | Débito     | Saldo      |");
-        System.out.println("+----------------+------------+------------+------------+");
-        System.out.println("| 03/01/2023     | 1200,00    | 0,00       | 1200,00    |");
-        System.out.println("| 15/02/2023     | 0,00       | 450,00     | 750,00     |");
-        System.out.println("| 10/03/2023     | 500,00     | 0,00       | 1250,00    |");
-        System.out.println("| 25/03/2023     | 0,00       | 1300,00    | -50,00     |");
-        System.out.println("| 04/04/2023     | 600,00     | 0,00       | 550,00     |");
-        System.out.println("+----------------+------------+------------+------------+");
-        System.out.println("| Totais         | 2300,00    | 1750,00    | 550,00     |");
-        System.out.println("+----------------+------------+------------+------------+");
+
+    private PagarController pagarController = new PagarController();
+    private ReceberController receberController = new ReceberController();
+
+    public void menuFluxoCaixa() {
+        
+        for (int i = 1; i <= 5; i++) {
+            pagarController.incluir(PagarFaker.gerarPagar());
+        }
+        
+        for (int i = 1; i <= 5; i++) {
+            receberController.incluir(ReceberFaker.gerarReceber());
+        }
+        
+        if (receberController.getRecebimentos().isEmpty() && pagarController.getContasPagar().isEmpty()) {
+            System.out.println("\n-----------------------------------------------");
+            System.out.println("             Não há dados no fluxo de caixa.   ");
+            System.out.println("-----------------------------------------------");
+        } else {
+            System.out.println("-----------------------------------------------");
+            System.out.println("                   Fluxo de Caixa              ");
+            System.out.println("-----------------------------------------------");
+            System.out.println("Vencimento       | Entrada   | Saída    | Saldo");
+            System.out.println("-----------------------------------------------");
+
+            double totalReceber = 0;
+            double totalPagar = 0;
+
+            if (!receberController.getRecebimentos().isEmpty()) {
+                for (ReceberModel receber : receberController.getRecebimentos()) {
+                    totalReceber += receber.getTotal();
+                    System.out.printf("%-17s | %-9.2f | %-8s | %-8.2f\n", receber.getVencimento(), receber.getTotal(), "0.00", totalReceber);
+                }
+            } else {
+                System.out.printf("%-17s | %-9s | %-8s | %-8s\n", "Vazio", "0.00", "0.00", "0.00");
+            }
+
+            if (!pagarController.getContasPagar().isEmpty()) {
+                for (PagarModel pagar : pagarController.getContasPagar()) {
+                    totalPagar += pagar.getTotal();
+                    System.out.printf("%-17s | %-9s | %-8.2f | %-8.2f\n", pagar.getVencimento(), "0.00", pagar.getTotal(), totalReceber - totalPagar);
+                }
+            } else {
+                System.out.printf("%-17s | %-9s | %-8s | %-8s\n", "Vazio", "0.00", "0.00", "0.00");
+            }
+
+            System.out.printf("%-17s | %-9.2f | %-8.2f | %-8.2f\n", "Totais", totalReceber, totalPagar, (totalReceber - totalPagar));
+            System.out.println("-----------------------------------------------");
+        }
     }
 }
